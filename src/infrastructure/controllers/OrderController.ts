5f3df3b7d57fcd1ce35c172759b330b343fc3f82
DavidTufi120 <davizinhol_@Hotmail.com>
2025-06-07 15:06:01 -0300

    updated endpoint orders, filtred users, same users has products with array now

--- src/infrastructure/controllers/OrderController.ts
+++ src/infrastructure/controllers/OrderController.ts
@@ -12,13 +12,62 @@ export class OrderController {
     }
 
     async processOrdersFromText(rawData: string): Promise<void> {
+
         await this.processOrdersUseCase.execute(rawData);
     }
-
     async getOrders(_req: Request, res: Response): Promise<void> {
+
+        const { order_id, start_id, end_date } = req.query;
+
         try {
             const orders = await this.orderRepository.findAll();
-            res.status(200).json(orders);
+
+            const usersMap = new Map<number, any>();
+
+            for (const order of orders) {
+                const userId = order.getUserId();
+                const userName = order.getUserName();
+                const orderId = order.getOrderId();
+                const productId = order.getProductId();
+                const value = Number(order.getValue());
+                const date = order.getDate();
+
+                const formattedDate = date.toISOString().split('T')[0];
+
+                if (!usersMap.has(userId)) {
+                    usersMap.set(userId, {
+                        user_id: userId,
+                        name: userName,
+                        orders: []
+                    });
+                }
+
+                const user = usersMap.get(userId);
+
+                let userOrder = user.orders.find((o: any) => o.order_id === orderId);
+
+                if (!userOrder) {
+                    userOrder = {
+                        order_id: orderId,
+                        total: 0,
+                        date: formattedDate,
+                        products: []
+                    };
+                    user.orders.push(userOrder);
+                }
+
+                userOrder.products.push({
+                    product_id: productId,
+                    value: value.toFixed(2)
+                });
+
+                // Soma o valor ao total do pedido
+                userOrder.total = (Number(userOrder.total) + value).toFixed(2);
+            }
+
+            const result = Array.from(usersMap.values());
+
+            res.status(200).json(result);
         } catch (error) {
             res.status(500).json({ error: 'Failed to retrieve orders' });
         }
